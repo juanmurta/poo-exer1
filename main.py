7e9f882710d35ec63022f6cc0db9a765ed6d85db
juanmurta <juanmurta@gmail.com>
2025-02-12 17:53:29 -0300

    upload

---
+++ main.py
@@ -0,0 +1,52 @@
+# Criando nossa 1ª Classe em Python
+# Sempre que você quiser criar uma classe, você vai fazer:
+#
+# class Nome_Classe:
+#
+# Dentro da classe, você vai criar a "função" (método) __init__
+# Esse método é quem define o que acontece quando você cria uma instância da Classe
+#
+# Vamos ver um exemplo para ficar mais claro, com o caso da Televisão que a gente vinha comentando
+
+# classes
+class TV:
+
+    def __init__(self, mark):
+        self.cor = 'preta'
+        self.ligada = False
+        self.tamanho = 55
+        self.canal = 'Netflix'
+        self.volume = 10
+        self.marca = mark
+
+    def mudar_canal(self):
+        self.canal = 'Disney+'
+
+    def alterar_canal(self, novo_canal):
+        self.canal = novo_canal
+        print(f'Canal altera para o {novo_canal}')
+
+
+# programa
+tv_sala = TV(mark='samsung')
+tv_quarto = TV('aoc')
+
+tv_sala.cor = 'Branca'
+
+print(tv_sala.cor)
+print(tv_quarto.canal)
+
+tv_quarto.tamanho = 32
+print(tv_quarto.tamanho)
+print(tv_sala.tamanho)
+
+print(tv_sala.canal)
+tv_sala.mudar_canal()
+print(tv_sala.canal)
+
+print(tv_quarto.canal)
+tv_quarto.alterar_canal('HBO')
+print(tv_quarto.canal)
+
+print(tv_quarto.marca)
+print(tv_sala.marca)
